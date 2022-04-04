d27499bc9e9c8721d89c2cb12f2485442661f5bc
Karim <karim.idrisow2016@gmail.com>
2022-04-04 22:51:10 +1000

    fix menu bug

--- src/layout/mobileLayout/components/MenuList/MenuList.js
+++ src/layout/mobileLayout/components/MenuList/MenuList.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import menuStore from '../../../../store/modules/menuStore'
 import MenuListItem from './MenuListItem';
-import { Skeleton, TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, CardContent, CircularProgress, Modal, Tab, Tabs, Typography } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { CircularProgress, Tab } from "@mui/material";
 import userStore from "../../../../store/modules/userStore";
 
 import s from './MenuList.module.scss';
 import TopHeader from './MenuListHeader';
 
-const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, receiveAddons, addons }) => {
+const MenuList = ({ menu, receiveMenu, coffeeHouse, changeCoffeeHouse, receiveAddons, addons }) => {
 
     const [value, setValue] = useState('coffee');
     const handleChange = (event, newValue) => {
@@ -17,17 +17,27 @@ const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, rece
     };
 
     const [search, setSearch] = useState('');
-    const [activeMenu, setActiveMenu] = useState([])
+    const [activeMenu, setActiveMenu] = useState(menu)
 
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (menu === []) {
+            setLoading(true);
+        } else {
+            setLoading(false);
+            setActiveMenu(menu)
+        }
+    }, [menu])
     useEffect(() => {
         const getMenu = async () => {
             setLoading(true);
             await receiveMenu();
-            setActiveMenu(menu)
+            await receiveAddons();
             setLoading(false);
         }
         if (search === '') {
             getMenu();
+            setActiveMenu(menu)
             return;
         }
         setActiveMenu(menu.filter((product) => product.name.toLowerCase().includes(search.toLowerCase())));
@@ -36,18 +46,6 @@ const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, rece
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-
-        const getMenuAndAddons = async () => {
-            setLoading(true);
-            await receiveMenu();
-            await receiveAddons();
-            setLoading(false);
-        }
-        getMenuAndAddons();
-    }, [])
 
     const changeActiveCoffeeHouse = (coffeeHouse) => {
         changeCoffeeHouse(coffeeHouse)
@@ -62,7 +60,7 @@ const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, rece
             short: 'E6'
         },
         {
-            id: '2',
+            id: '1',
             title: 'Полка кофе',
             address: 'Корпус D, 7 этаж \n Открыто до 20:00',
             short: 'D7'
@@ -93,7 +91,7 @@ const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, rece
                     </div>
                     <div className='mb65-container'>
                         <TabPanel value='coffee'>
-                            {loading
+                            {loading && activeMenu.length > 0
                                 ? (
                                     <div className={s.spinner}>
                                         <CircularProgress color="success" />
@@ -101,7 +99,7 @@ const MenuList = ({ menu = [], receiveMenu, coffeeHouse, changeCoffeeHouse, rece
                                 )
                                 : activeMenu.map((product) => {
                                     return (
-                                        <MenuListItem key={product.id} item={product} addons={addons}/>
+                                        <MenuListItem key={product.id} item={product} addons={addons} />
                                     )
                                 })}
                         </TabPanel>
